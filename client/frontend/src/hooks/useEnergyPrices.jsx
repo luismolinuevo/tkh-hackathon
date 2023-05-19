4af2ba9ff25b4535d193853d376ce0480dcb1b13
Adil Mezghouti <amezghouti@bloomberg.net>
2023-05-19 12:56:24 -0400

    Added another hook to read energy prices

---
+++ client/frontend/src/hooks/useEnergyPrices.jsx
@@ -0,0 +1,29 @@
+import {useEffect, useState} from "react";
+
+const useEnergyPrices = () => {
+    const URL = `https://hourlypricing.comed.com/api?type=currenthouraverage`
+    const [priceInfo, setPriceInfo] = useState({loading: true})
+
+    useEffect(() => {
+        const successCallback = async (response) => {
+            const data = await response.json()
+            setPriceInfo({
+                loading: false,
+                price: `${data[0]?.price} Cents/kHw`
+            })
+        }
+
+        const errorCallback = (error) => {
+            setPriceInfo({
+                loading: false,
+                error
+            })
+        }
+
+        fetch(URL).then(successCallback).catch(errorCallback)
+    }, [])
+
+    return priceInfo
+}
+
+export default useEnergyPrices

4682b82d1c889c6afdb6229a9442db4d6f82118c
Adil Mezghouti <amezghouti@bloomberg.net>
2023-05-19 11:42:49 -0400

    Added another hook to read weather data

--- client/frontend/src/hooks/useUserLocation.jsx
+++ client/frontend/src/hooks/useUserLocation.jsx
@@ -1,29 +1,30 @@
 import {useEffect, useState} from "react";
 
 const useUserLocation = () => {
-    const [city, setCity] = useState()
-    const [state, setState] = useState()
-    const [error, setError] = useState()
-    const [loading, setLoading] = useState(true)
+    const [locationInfo, setLocationInfo] = useState({loading: true})
 
     useEffect(() => {
         const successCallback = async ({coords}) => {
-            console.log(coords)
             const {latitude, longitude} = coords
             const location = await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`)
             const {address: {city, state}} = await location.json()
-            setLoading(false)
-            setCity(city)
-            setState(state)
+            setLocationInfo({
+                loading: false,
+                city,
+                state
+            })
         }
         const errorCallback = (error) => {
-            setError(error)
+            setLocationInfo({
+                loading: false,
+                error
+            })
         }
 
         navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-    })
+    }, [])
 
-    return {city, state, error, loading}
+    return locationInfo
 
 }
 

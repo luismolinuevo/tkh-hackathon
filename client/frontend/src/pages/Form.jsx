0bf8f3e9baa28d5ae7da226ac27cd84ce49770d6
Jacky <jackypengsmail@gmail.com>
2023-05-19 11:05:43 -0400

    Update tailwind config, created navbar and pages

---
+++ client/frontend/src/pages/Form.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>This is the form page.</div>
+  )
+}
+
+export default Form
---
+++ client/frontend/src/pages/Posts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Posts = () => {
+  return (
+    <div>This is where user posts are.</div>
+  )
+}
+
+export default Posts

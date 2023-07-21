f30466f3e95da79ba209efa2df4eecbfee2efd85
Luis Molinuevo <89353175+luismolinuevo@users.noreply.github.com>
2023-07-21 02:57:04 -0400

    redux setup frontend login logic working just needs styles

--- client/frontend/src/pages/Login.jsx
+++ client/frontend/src/pages/Login.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { useState } from "react";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser } from "../redux/auth";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const isAuthed = useSelector((state) => state.auth.isLoggedIn)
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(loginUser(username, password));
+    if(isAuthed) {
+      navigate("/")
+    }
+  };
+
   return (
-    <div>This is the Login.</div>
+    <div>
+      <div className='bg-[#E5EBE1]'>
+        <h1>Welcome Back</h1>
+        <form action="">
+          <input type="text" onChange={e => setUsername(e.target.value)}/>
+          <input type="text"onChange={e => setPassword(e.target.value)}/>
+          <button onClick={handleLogin}>Log In</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
---
+++ client/frontend/src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit"
+import auth from "./auth";
+
+const store = configureStore({
+    reducer: {  //may have to add .reducer
+        auth: auth,
+    },
+  });
+  
+  export default store;
